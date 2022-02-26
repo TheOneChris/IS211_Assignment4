753417ea88f48e86081bf68b319e802cdf6e2713
TheOneChris <60333802+TheOneChris@users.noreply.github.com>
2022-02-26 11:40:10 -0500

    updated
    finished I think?

--- search_compare.py
+++ search_compare.py
@@ -2,7 +2,7 @@ import argparse
 # other imports go here
 
 import random
-
+import time
 def get_me_random_list(n):
     """Generate list of n elements in random order
     
@@ -14,21 +14,102 @@ def get_me_random_list(n):
     return a_list
     
 def sequential_search(a_list,item):
-    pass
+    """Copied From Book / Website"""
+    pos = 0
+    found = False
 
+    start = time.time()
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+    end = time.time()
+    sort_time = end - start
+
+    return (found, sort_time)
 
 def ordered_sequential_search(a_list,item):
-    pass
+    """Copied From Book / Website"""
+    pos = 0
+    found = False
+    stop = False
+
+    start = time.time()
+    while pos < len(a_list) and not found and not stop:
+        if a_list[pos] == item:
+            found = True
+        else:
+            if a_list[pos] > item:
+                stop = True
+            else:
+                pos = pos + 1
+    end = time.time()
+    sort_time = end - start
+
+    return(found, sort_time)
 
 
 def binary_search_iterative(a_list,item):
-    pass
+    """Copied from Book / Website"""
+    start = time.time()
+    first = 0
+    last = len(a_list) - 1
+    found = False
+
+    start = time.time()
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+    end = time.time()
+    sort_time = end - start
+    return (found, sort_time)
     
     
 def binary_search_recursive(a_list,item):
-    pass
+    """Copied from Book / Website"""
+    start = time.time()
+
+    if len(a_list) == 0:
+        end = time.time()
+        sort_time = end - start
+        return (False, sort_time)
+    else:
+        midpoint = len(a_list) // 2
+    if a_list[midpoint] == item:
+        end = time.time()
+        sort_time = end - start
+        return (True, sort_time)
+    else:
+        if item < a_list[midpoint]:
+            return binary_search_recursive(a_list[:midpoint], item)
+        else:
+            return binary_search_recursive(a_list[midpoint + 1:], item)
 
 
 if __name__ == "__main__":
     """Main entry point"""
-    pass
+    random.seed(100)
+    searching = {"Sequential Search": sequential_search, "Ordered Sequential Search": ordered_sequential_search,
+                "Binary Search Iterative": binary_search_iterative, "Binary search recursive": binary_search_recursive}
+
+    list_size = [500, 1000, 5000]
+
+    for search in searching:
+        for lists in list_size:
+            total_time = 0
+            for i in range(100):
+                sorted_list = sorted(get_me_random_list(lists))
+                results = searching[search](sorted_list, 99999999)
+                total_time += results[1]
+            avg_time = total_time / 100
+            print(f"List Size:{lists}: {search} took {avg_time:10.7f} seconds to run on average")
+
+"""Unsure if the way the assignments wants but I believe I got right answers"""
--- sort_compare.py
+++ sort_compare.py
@@ -2,6 +2,7 @@ import argparse
 # other imports go here
 
 import random
+import time
 
 def get_me_random_list(n):
     """Generate list of n elements in random order
@@ -15,16 +16,59 @@ def get_me_random_list(n):
     
 
 def insertion_sort(a_list):
-    pass
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position - 1]
+            position = position - 1
+    a_list[position] = current_value
+    return a_list
 
 
 def shell_sort(a_list):
-    pass
+    sublist_count = len(a_list) // 2
+    while sublist_count > 0:
+        for start_position in range(sublist_count):
+            gap_sort(a_list, start_position, sublist_count)
+        sublist_count = sublist_count // 2
+    return a_list
+
+
+def gap_sort(a_list, start, gap):
+    '''Added this from book / website, it is a Subfunction for Shell sort'''
+    for i in range(start + gap, len(a_list), gap):
+        current_value = a_list[i]
+        position = i
+    while position >= gap and a_list[position - gap] > current_value:
+        a_list[position] = a_list[position - gap]
+        position = position - gap
+    a_list[position] = current_value
 
 
 def python_sort(a_list):
-    pass
+    return sorted(a_list)
+
+
+def average_timing(sortFunc, Comment):
+    random.seed(100)
+
+    list_size = [500, 100, 5000]
+    total_time = 0
+    for list_size in list_size:
+        for i in range(100):
+            start = time.time()
+            sortFunc(get_me_random_list(list_size))
+            end = time.time()
+            sort_time = end - start
+            total_time += sort_time
+
+        avg_time = total_time / 100
+        print(f"{Comment} took on average {avg_time:10.7f} seconds to run for the list size {list_size}")
+
 
 if __name__ == "__main__":
     """Main entry point"""
-    pass
+    average_timing(insertion_sort, "Insertion Sort")
+    average_timing(python_sort, "Python Sort")
+    average_timing(shell_sort, "Shell Sort")
